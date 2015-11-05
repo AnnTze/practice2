5fa82402bb86fad0c492a6e1bc3a916f5ff22138
tananntze <tananntze@gmail.com>
2015-11-05 17:46:06 +0800

    Second Commit

--- src/demo/BookDBAO.java
+++ src/demo/BookDBAO.java
@@ -1,5 +1,9 @@
 package demo;
 
+import java.sql.*;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  * Created by tananntze on 31/10/2015.
  */
@@ -7,8 +11,24 @@ public class BookDBAO {
     public BookDetails getBookDeatails(String bookId){
         return createDummyBook(bookId);
     }
+    Connection con;
+    public static String url = "jdbc:mysql://localhost/test";
+    public static String dbdriver = "com.mysql.jdbc.Driver";
+    public static String username = "root";
+    public static String password = "24111997nOv";
+
+    public BookDBAO() throws Exception{
+        Class.forName(dbdriver);
+        con = DriverManager.getConnection(url,username,password);
+    }
+
+    public void getConnection() throws SQLException{
+        if(con == null)con = DriverManager.getConnection(url,username,password);
+    }
+
 
     private BookDetails createDummyBook(String bookId){
+
         BookDetails details = new BookDetails();
         details.setBookId(bookId);
         details.setDescription("Web components for developers talks about how web components can transfer the way you develop application for the web");
