77366c6fae197cf9fbe776a634bedf768a14f919
tananntze <tananntze@gmail.com>
2015-11-09 09:19:35 +0800

    Second Commit

--- src/demo/BookCatalogServlet.java
+++ src/demo/BookCatalogServlet.java
@@ -20,39 +20,17 @@ public class BookCatalogServlet extends HttpServlet {
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        PrintWriter out = response.getWriter();
         try {
-            BookDBAO db = new BookDBAO();
-            List<BookDetails> list = db.getAllBook();
-            out.println("<html>" + "<head><title>Duke's Bookstore</title></head>" +
-                    "<body  bgcolor=\"#ffffff\">" + "<center>" +
-                    "<hr> <br> &nbsp;" + "<h1>" +
-                    "<font size=\"+3\" color=\"#CC0066\">Duke's </font> <img src=\"" +
-                    "./duke.books.gif\" alt=\"Duke holding books\"\">" +
-                    "<font size=\"+3\" color=\"black\">Bookstore</font>" + "</h1>" +
-                    "</center>" + "<br> &nbsp; <hr> <br> ");
-
-            out.println("<br> &nbsp;" + "<h3>Please Choose from our selection" +
-                    "</h3>" + "<center> <table summary=\"layout\">");
-
-            for (BookDetails book : list) {
-                String bookId = book.getBookId();
-                out.println("<tr>" + "<td bgcolor=\"#ffffaa\">" + "<a href=\"" +
-                        response.encodeURL(request.getContextPath() +
-                                "/bookdetails?bookId=" + bookId) + "\"> <strong>" +
-                        book.getTitle() + "&nbsp; </strong></a></td>" +
-                        "<td bgcolor=\"#ffffaa\" rowspan=2>" + book.getPrice() +
-                        "&nbsp; </td>" + "<td bgcolor=\"#ffffaa\" rowspan=2>" +
-                        "<a href=\"" +
-                        response.encodeURL(request.getContextPath() +
-                                "/bookcatalog?bookId=" + bookId) + "\"> &nbsp;" +
-                        "Add to Cart&nbsp;</a></td></tr>" +
-                        "<tr>" + "<td bgcolor=\"#ffffff\">" + "&nbsp; &nbsp;" +
-                        "by<em> " + book.getFirstName() +
-                        " " + book.getSurname() + "</em></td></tr>");
+            String bookId = request.getParameter("bookId");
+            if (bookId != null) {
+                BookDBAO db = new BookDBAO();
+                BookDetails bd = db.getBookDetails(bookId);
+                // store bookdetails object in request scope with attribut name "book"
+                // bookdetails.jsp will retrieve this stored object later to display the content
+                request.setAttribute("book", bd);
+                // forward the request to bookdetails.jsp, there must be a "/" before the web resource reference
+                getServletContext().getRequestDispatcher("/bookdetails.jsp").forward(request,response);
             }
-
-            out.println("</table></center></body></html>");
         } catch (Exception e) {
             e.printStackTrace();
             throw new ServletException(e);
